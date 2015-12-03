d874df900a36ed25ead05d6178e14f60c4f6280e
Ducas Francis <ducas@ducasfrancis.com>
2015-12-03 22:04:36 +1100

    use default partitioning strategy to put keyed messages on the right partition
    - build a payload for each message that is keyed if the message includes a key
    - refresh the topic's metadata to get number of partitions
    - use murmur2 to hash the key and determine partition using modulus of num partitions

--- controllers/topics.js
+++ controllers/topics.js
@@ -1,8 +1,20 @@
 var kafka = require('kafka-node'),
+    murmur = require('murmurhash-js'),
     config = require('config-node')(),
     client = new kafka.Client(config.kafka.zkConnect, 'kafka-rest-proxy'),
     producer = new kafka.HighLevelProducer(client),
-    compression = config.kafka.compression || 0;
+    compression = config.kafka.compression || 0,
+    seed = 0x9747b28c,
+
+    refreshTopic = function (topic, cb) {
+        client.topicExists([topic], function (err, data) {
+            if (err) return cb(err);
+            client.refreshMetadata([topic],  function (err, data) {
+                if (err) return cb(err);
+                cb();
+            });
+        });
+    };
 
 module.exports = function (app) {
 
@@ -17,19 +29,28 @@ module.exports = function (app) {
     });
 
     app.post('/topics/:topic', function (req, res) {
+        var topic = req.params.topic;
 
-        var messages = req.body.payload.map(function (p) {
-                return typeof(p.key) !== 'undefined'
-                    ? new kafka.KeyedMessage(p.key, p.value)
-                    : p.value;
+        refreshTopic(topic, function (err) {
+            if (err) return res.status(500).json({ error: err });
+
+            var numPartitions = client.topicPartitions[topic].length,
+                messages = req.body.payload.map(function (p) {
+                    return {
+                        topic: topic,
+                        messages: typeof(p.key) !== 'undefined'
+                            ? new kafka.KeyedMessage(p.key, p.value)
+                            : p.value,
+                        partition: typeof(p.key) !== 'undefined'
+                            ? murmur.murmur2(p.key, seed) % numPartitions
+                            : undefined
+                    };
+                });
+
+            producer.send(messages, function (err, data) {
+                if (err) return res.status(500).json({error: err});
+                res.json(data);
             });
-
-        producer.send([{
-            topic: req.params.topic,
-            messages: messages
-        }], function (err, data) {
-            if (err) return res.status(500).json({error: err});
-            res.json(data);
         });
 
     })
