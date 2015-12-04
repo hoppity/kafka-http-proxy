ea2d33a4c1ab5b25d9d2c5176f4cfb76bd6066b7
Ducas Francis <ducas@ducasfrancis.com>
2015-12-04 14:27:46 +1100

    add an access log

--- server.js
+++ server.js
@@ -2,20 +2,25 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     config = require('config-node')(),
-    logger = require('./logger.js');
+    logger = require('./logger.js'),
 
+    fs = require('fs'),
+    morgan = require('morgan'),
+    accessLogStream = fs.createWriteStream(__dirname + '/' + config.accessLogPath, {flags: 'a'});
 
-app.use(bodyParser.json());
+app.use(morgan('combined', { stream: accessLogStream }));
 
-app.use(function(err, req, res, next) {
-	if (err) {
-		logger.error({error: err, request: req, response: res});
-		next(err);
-	}
-});
+app.use(bodyParser.json({ type: 'application/*+json' }));
 
 require('./controllers/topics')(app);
 require('./controllers/consumers')(app);
+
+app.use(function(err, req, res, next) {
+    if (err) {
+        logger.error({error: err, request: req, response: res});
+        next(err);
+    }
+});
 
 app.use(function errorHandler(err, req, res, next) {
     console.error(err);
