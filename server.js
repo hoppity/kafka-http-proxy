bf5b5859b6d5b5bb3e35a64468e2f63771c60e01
Ducas Francis <ducas@ducasfrancis.com>
2015-12-04 11:33:37 +1100

    write all errors as json

--- server.js
+++ server.js
@@ -8,4 +8,12 @@ app.use(bodyParser.json());
 require('./controllers/topics')(app);
 require('./controllers/consumers')(app);
 
+app.use(function errorHandler(err, req, res, next) {
+    console.error(err);
+    if (res.headersSent) {
+        return next(err);
+    }
+    res.status(500).json({ 'error_code': 500, 'message': err });
+});
+
 app.listen(config.port);
