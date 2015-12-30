8c9e1438dd441b03e78d70876aca03ffed03f6b9
Ducas Francis <ducas@ducasfrancis.com>
2015-12-30 21:05:19 +1100

    setup stdout and file streams for logging to, but disable file in default and development

--- logger.js
+++ logger.js
@@ -1,15 +1,31 @@
 var bunyan = require('bunyan');
 var config = require('./config');
+var PrettyStream = require('bunyan-prettystream');
+ 
+var prettyStdOut = new PrettyStream();
+prettyStdOut.pipe(process.stdout);
+
+var streams = [];
+if (config.logging.stdout && config.logging.stdout.enabled) {
+    streams.push({
+        level: config.logging.stdout.level,
+        type: 'raw',
+        stream : prettyStdOut
+    });
+}
+if (config.logging.file && config.logging.file.enabled) {
+    streams.push({
+        level: config.logging.file.level,
+        path: config.logging.file.path,
+        type: 'file'
+    });
+}
 
 var logger = new bunyan.createLogger({
-        name: config.logging.logName,
-        src:true,
-        streams: [
-            {
-                level: config.logging.level,
-                stream : process.stdout
-            }]
-    });
+    name: config.logging.logName,
+    src: true,
+    streams: streams
+});
 
 module.exports = {
     logger : logger,
