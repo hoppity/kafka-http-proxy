742282453fb85d031985d29f1dc65429d57c9aee
Ducas Francis <ducas@ducasfrancis.com>
2016-01-05 15:31:14 +1100

    allow port number to be overriden by environment variable

--- config.js
+++ config.js
@@ -7,4 +7,8 @@ if (!!process.env.ZOOKEEPER_CONNECT) {
     config.kafka.zkConnect = process.env.ZOOKEEPER_CONNECT;
 }
 
+if (!!process.env.PORT) {
+   config.port = process.env.PORT;
+}
+
 module.exports = config;
